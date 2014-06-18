288f5bf422bc573ac2573946073698dce65e61ca
megha <megha_64@yahoo.com>
2014-06-18 17:24:21 +0530

    Added pop up for sign up and added login form...

--- script/app.js
+++ script/app.js
@@ -1,32 +1,28 @@
-var taClassified = angular.module('taClassified', ['ngRoute']);
+var taClassified = angular.module('taClassified', ['ngRoute','ngDialog']);
 
-/* 
-** TODO - create separate file
-*/
+//Navigation Controller
 
-taClassified.config(['$routeProvider',function($routeProvider) {
-	$routeProvider
-		.when('/', { templateUrl: 'template/home.html', controller: 'taCtrl'})
-		.when('/register', { templateUrl: 'template/register.html', controller: 'signupCtrl'});
-}]);
+taClassified.controller('navCtrl', ['$scope','ngDialog', function($scope, ngDialog){
+	$scope.signUp = function () {
+		ngDialog.open({
+			template: 'template/register.html',
+			className: 'ngdialog-theme-default',
+		});			
+	};	
+	$scope.logIn = function () {
+		ngDialog.close();
+		ngDialog.open({
+			template: 'template/login.html',
+			className: 'ngdialog-theme-default',
+		});			
+	};	
+}])
 
-/* 
-** TODO - rename taCtrl
-*/
+// Form Controller
 
-taClassified.controller('taCtrl', ['$scope','$http', function($scope, $http){
-	$http({method: 'GET', cache: false, url: "data/ads.json"})
-		.success(function(data, status, headers, config){
-			$scope.ads = data;
-		})
-		.error(function(data, status){
-			console.log('error'+ status);
-		});
-}]);
-
-// Signup controller
-taClassified.controller('signupCtrl', ['$scope', function($scope) {
-	  $scope.submitted = false;
+taClassified.controller('formCtrl', ['$scope', function($scope){
+	 $scope.submitted = false;
+	 //register form validation
 	  $scope.submitForm = function() {
 	    if ($scope.signupForm.$valid) {
 	      // Submit as normal
@@ -36,7 +32,41 @@ taClassified.controller('signupCtrl', ['$scope', function($scope) {
 	    	$scope.signupForm.submitted = true;
 	    	console.log("error");
 	}
-}]); 
+	//log in form validation
+	$scope.submitLogIn = function() {
+	    if ($scope.loginForm.$valid) {
+	      // Submit as normal
+	      console.log("done");
+	    }
+	    else
+	    	$scope.loginForm.submitted = true;
+	    	console.log("error");
+	}
+}])
+/* 
+** TODO - create separate file
+*/
+
+taClassified.config(['$routeProvider',function($routeProvider) {
+	$routeProvider
+		.when('/', { templateUrl: 'template/home.html', controller: 'taCtrl'});
+}]);
+
+/* 
+** TODO - rename taCtrl
+*/
+
+taClassified.controller('taCtrl', ['$scope','$http' ,function($scope, $http){
+	$http({method: 'GET', cache: false, url: "data/ads.json"})
+		.success(function(data, status, headers, config){
+			$scope.ads = data;
+		})
+		.error(function(data, status){
+			console.log('error'+ status);
+		});		
+}]);
+
+
 
 // Filter skeleton 
 taClassified.filter('capitalise', function(){
